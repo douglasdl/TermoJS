28e6480bd4da208eda99a7f8fc40a1d91b4fa4cb
douglasdl <douglas_san@hotmail.com>
2022-05-29 01:17:16 -0300

    Update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
 const messageDisplay = document.querySelector('.message-container');
-const word = 'HELLO';
+const word = 'POEMA';
 
 const keys = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
@@ -22,6 +22,24 @@ let currentRow = 0;
 let currentTile = 0;
 let isGameOver = false;
 
+
+const updateActiveTile = (isDeleting = false) => {
+    const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+    tile.classList.add('activeTile');
+    if(!isDeleting) {
+        if(currentTile > 0) {
+            const previousTile = document.getElementById('guessRow-' + currentRow + '-tile-' + (currentTile - 1));
+            previousTile.classList.remove('activeTile');
+        }
+    }
+    if(currentTile < 5) {
+        const previousTile = document.getElementById('guessRow-' + currentRow + '-tile-' + (currentTile + 1));
+        previousTile.classList.remove('activeTile');
+    }
+}
+
+
+// Create the tiles in the screen
 guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div');
     rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
@@ -34,6 +52,8 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     });
 
     tileDisplay.append(rowElement);
+
+    updateActiveTile();
 });
 
 const addLetter = (letter) => {
@@ -43,6 +63,7 @@ const addLetter = (letter) => {
         guessRows[currentRow][currentTile] = letter;
         tile.setAttribute('data', letter);
         currentTile++;
+        updateActiveTile();
         console.log('guessRows: ', guessRows);
     }
 }
@@ -50,6 +71,7 @@ const addLetter = (letter) => {
 const deleteLetter = () => {
     if(currentTile > 0) {
         currentTile--;
+        updateActiveTile(isDeleting = true);
         const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
         tile.textContent = '';
         guessRows[currentRow][currentTile] = '';
@@ -73,21 +95,47 @@ const addColorToKey = (keyLetter, color) => {
 
 const flipTile = () => {
     const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes;
+    let checkWord = word;
+    const guess = [];
+    rowTiles[4].classList.remove('activeTile');
+
+    rowTiles.forEach((tile) => {
+        guess.push({ letter: tile.getAttribute('data'), color: 'grey-overlay' });
+    });
+
+    guess.forEach((guess, index) => {
+        if(guess.letter === word[index]) {
+            guess.color = 'green-overlay';
+            checkWord = checkWord.replace(guess.letter, '');
+        }
+    });
+
+    guess.forEach((guess) => {
+        if(checkWord.includes(guess.letter)) {
+            guess.color = 'yellow-overlay';
+            checkWord = checkWord.replace(guess.letter, '');
+        }
+    });
+
     rowTiles.forEach((tile, index) => {
         const dataLetter = tile.getAttribute('data');
 
         setTimeout(() => {
             tile.classList.add('flip');
-            if(dataLetter == word[index]) {
-                tile.classList.add('green-overlay');
-                addColorToKey(dataLetter, 'green-overlay');
-            } else if(word.includes(dataLetter)) {
-                tile.classList.add('yellow-overlay');
-                addColorToKey(dataLetter, 'yellow-overlay');
-            } else {
-                tile.classList.add('grey-overlay');
-                addColorToKey(dataLetter, 'grey-overlay');
-            }
+            tile.classList.add(guess[index].color);
+            addColorToKey(guess[index].letter, guess[index].color);
+
+            // tile.classList.add('flip');
+            // if(dataLetter == word[index]) {
+            //     tile.classList.add('green-overlay');
+            //     addColorToKey(dataLetter, 'green-overlay');
+            // } else if(word.includes(dataLetter)) {
+            //     tile.classList.add('yellow-overlay');
+            //     addColorToKey(dataLetter, 'yellow-overlay');
+            // } else {
+            //     tile.classList.add('grey-overlay');
+            //     addColorToKey(dataLetter, 'grey-overlay');
+            // }
         }, 500 * index);
     });
 }
@@ -111,6 +159,7 @@ const checkRow = () => {
             if(currentRow < 5) {
                 currentRow++;
                 currentTile = 0;
+                updateActiveTile();
             }
         }
     }
